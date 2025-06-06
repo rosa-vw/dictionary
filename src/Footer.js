9fd5b9d88e22212e756d7a7807621d92f33313d3
Rosa van Wouwe <rosavanwouwe@hotmail.com>
2025-06-06 17:08:52 +0200

    Fix links

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,23 @@ function Footer() {
     <div className="Footer">
       <p>
         This project was coded by{" "}
-        <a href="https://github.com/rosa-vw" target="_blank">
+        <a href="https://github.com/rosa-vw" target="_blank" rel="noopener">
           Rosavw
         </a>
         , is open-sourced on{" "}
-        <a href="https://github.com/rosa-vw/dictionary" target="_blank">
+        <a
+          href="https://github.com/rosa-vw/dictionary"
+          target="_blank"
+          rel="noopener"
+        >
           GitHub
         </a>{" "}
         , hosted on{" "}
-        <a href="https://dictionary-rosavw.netlify.app/" target="_blank">
+        <a
+          href="https://dictionary-rosavw.netlify.app/"
+          target="_blank"
+          rel="noopener"
+        >
           Netlify
         </a>{" "}
         and uses the SheCodes Dictionary API
